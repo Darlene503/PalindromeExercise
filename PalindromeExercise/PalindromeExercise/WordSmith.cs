483f0b381fb22c18df4da35db465589df5d8d282
Darlene503 <duyn503@gmail.com>
2023-08-17 06:38:55 -0700

    completed

--- PalindromeExercise/PalindromeExercise/WordSmith.cs
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace PalindromeExercise
 {
-    internal class wordsmith
+    public class WordSmith
     {
+        public bool IsAPalindrome(string word)
+        {
+            var wordCharArr = word.ToCharArray();
+            Array.Reverse(wordCharArr);
+            var reversed = new string(wordCharArr);
+            if(word.ToLower()== reversed.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
     }
 }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("level", true)]
+        [InlineData("civic", true)]
+        [InlineData("Scoobie", false)]
+        public void PalindromeTest(string word, bool expected)
         {
+            //Arr
+            var wordSmith = new WordSmith();
 
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //Asst
+            Assert.Equal(expected, actual);
         }
     }
 }
